e73ac9a1de1e1849aab66e4d51f4b439cfb6a585
Thandile Xiphu <thandilexiphu@ThandileXiphu-MBP.local>
2019-08-22 16:38:39 +0200

    initial code on my profilePage

---
+++ src/containers/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class ProfilePage extends React.Component {
+    render() {
+        return (
+            <div className='profilePage'>
+                <div className='profilePageContainer'>
+                    Loading
+                </div>
+            </div>
+        )
+    }
+}
+export default ProfilePage;
